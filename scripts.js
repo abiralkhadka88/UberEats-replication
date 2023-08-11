93b8ef3a2bf29a36e89b5acacece770d94e6a5d3
Abiral88 <abiralkhadka88@gmail.com>
2023-08-11 07:33:33 +0545

    Landing Page

---
+++ scripts.js
@@ -0,0 +1,33 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction();
+};
+
+// Get the header
+var header = document.querySelector("nav");
+var search_bar = document.querySelector(".search-bar");
+var foodBtn = document.querySelector(".find-food-elements");
+// console.log(header);
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+var foodBtn = foodBtn.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+
+  if (window.pageYOffset > foodBtn) {
+    if (window.innerWidth > 768) {
+      search_bar.style.display = "flex";
+      document.querySelector(".bg-white").style.backgroundColor = "#eee";
+    }
+  } else {
+    search_bar.style.display = "none";
+  }
+}
